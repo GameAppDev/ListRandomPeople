ef23d71d6f4a7344a318cfb1060ec42e1f7e3b55
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 20:27:56 +0300

    AppDelegate is updated.

--- ListRandomPeople/ListRandomPeople/Application/AppDelegate.swift
+++ ListRandomPeople/ListRandomPeople/Application/AppDelegate.swift
@@ -9,12 +9,20 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        window = UIWindow(frame: UIScreen.main.bounds)
+        openApp()
         
         return true
+    }
+    
+    private func openApp() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootVC = ListPeopleRouter().returnVC()
+        let navController = UINavigationController(rootViewController: rootVC)
+        
+        window.rootViewController = navController
+        window.makeKeyAndVisible()
     }
 }
