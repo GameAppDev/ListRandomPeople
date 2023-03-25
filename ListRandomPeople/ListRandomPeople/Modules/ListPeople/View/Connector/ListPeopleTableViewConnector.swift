e7270fc27c6834e180e36aab621905e363793ddd
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 21:09:09 +0300

    ListPeopleTableViewConnector class is implemented.

---
+++ ListRandomPeople/ListRandomPeople/Modules/ListPeople/View/Connector/ListPeopleTableViewConnector.swift
@@ -0,0 +1,51 @@
+//
+//  ListPeopleTableViewConnector.swift
+//  ListRandomPeople
+//
+//  Created by Oguzhan Yalcin on 25.03.2023.
+//
+
+import UIKit
+
+final class ListPeopleTableViewConnector: NSObject {
+    
+    private let presenter: IListPeopleConnectorToPresenter?
+    
+    init(presenter: IListPeopleConnectorToPresenter) {
+        self.presenter = presenter
+    }
+}
+
+extension ListPeopleTableViewConnector: UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Count
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter?.getPeopleList().count ?? 0
+    }
+    
+    // MARK: - Cells
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ListTableViewCell.className,
+                                                       for: indexPath) as? ListTableViewCell
+        else { return UITableViewCell() }
+
+        if let peopleList = presenter?.getPeopleList(),
+           let person = peopleList[safe: indexPath.row] {
+            cell.configureCell(person: person)
+        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter?.handleDetail(index: indexPath.row)
+    }
+}
