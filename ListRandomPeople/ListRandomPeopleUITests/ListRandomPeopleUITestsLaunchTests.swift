fd5aada61f943adcdb8ef638ef4096e05b8f8b12
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 17:21:07 +0300

    project init

---
+++ ListRandomPeople/ListRandomPeopleUITests/ListRandomPeopleUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ListRandomPeopleUITestsLaunchTests.swift
+//  ListRandomPeopleUITests
+//
+//  Created by Oguzhan Yalcin on 25.03.2023.
+//
+
+import XCTest
+
+final class ListRandomPeopleUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
