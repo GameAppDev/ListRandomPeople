a8e392be6f5a3b489a7d4136f1287fe6fee1c759
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 22:35:08 +0300

    response handle process is updated.

--- ListRandomPeople/ListRandomPeople/Modules/ListPeople/Interactor/ListPeopleInteractor.swift
+++ ListRandomPeople/ListRandomPeople/Modules/ListPeople/Interactor/ListPeopleInteractor.swift
@@ -12,12 +12,28 @@ final class ListPeopleInteractor {
     weak var presenter: IListPeopleInteractorToPresenter?
     
     private var peopleList: [Person] = []
+    private var nextNumber: String = "0"
 }
 
 extension ListPeopleInteractor: IListPeoplePresenterToInteractor {
     
     // MARK: Fetch
-    func fetchData<T>(request: T) { }
+    func fetchData<T>(request: T) {
+        DataSource.fetch(next: nextNumber) { [weak self] response, error in
+            guard let self = self else { return }
+            
+            if let response = response {
+                self.peopleList.append(contentsOf: response.people)
+                self.nextNumber = response.next ?? "0"
+                
+                self.presenter?.setData(data: self.peopleList)
+            } else if let error = error {
+                self.presenter?.setError(error: error.errorDescription)
+            } else {
+                self.presenter?.setError(error: "Try Again...")
+            }
+        }
+    }
     
     // MARK: Get
     func getPeopleList() -> [Person] {
