8c1a577b18cf77b8fb9d800871475696d623a1b0
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 18:28:17 +0300

    theme folder are created.

--- ListRandomPeople/ListRandomPeople/Theme/Colours.swift
+++ ListRandomPeople/ListRandomPeople/Theme/Colours.swift
@@ -5,4 +5,13 @@
 //  Created by Oguzhan Yalcin on 25.03.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    
+    static let viewBGColour      = UIColor(red: 50/255, green: 150/255, blue: 250/255, alpha: 1)
+    static let textColour        = UIColor(red: 60/255, green: 60/255, blue: 60/255, alpha: 1)
+    static let titleColour       = UIColor(red: 60/255, green: 60/255, blue: 160/255, alpha: 1)
+    static let navbarTextColour  = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static let navbarBGColour    = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+}
--- ListRandomPeople/ListRandomPeople/Theme/Fonts.swift
+++ ListRandomPeople/ListRandomPeople/Theme/Fonts.swift
@@ -5,4 +5,23 @@
 //  Created by Oguzhan Yalcin on 25.03.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    
+    static let navbarTextFont  = UIFont(name: AppFonts.bold.fontName, size: CGFloat(22).ws)
+    static let textFont        = UIFont(name: AppFonts.medium.fontName, size: CGFloat(14).ws)
+    static let titleFont       = UIFont(name: AppFonts.bold.fontName, size: CGFloat(16).ws)
+}
+
+enum AppFonts {
+    case bold
+    case medium
+    
+    var fontName: String {
+        switch self {
+        case .bold: return "Nunito-Bold"
+        case .medium: return "Nunito-Medium"
+        }
+    }
+}
