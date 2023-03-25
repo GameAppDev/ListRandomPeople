53ea0d590bd001f949d5f2d30638f67bc177fa2e
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 20:28:46 +0300

    ListPeopleBuilder is added.

--- ListRandomPeople/ListRandomPeople/Modules/ListPeople/Router/ListPeopleBuilder.swift
+++ ListRandomPeople/ListRandomPeople/Modules/ListPeople/Router/ListPeopleBuilder.swift
@@ -5,4 +5,24 @@
 //  Created by Oguzhan Yalcin on 25.03.2023.
 //
 
-import Foundation
+import UIKit
+
+struct ListPeopleBuilder {
+
+    static func buildModule() -> UIViewController {
+        let viewController = ListPeopleViewController()
+        let interactor = ListPeopleInteractor()
+        let router = ListPeopleRouter()
+        let presenter = ListPeoplePresenter(view: viewController,
+                                            interactor: interactor,
+                                            router: router)
+        //TODO: add tableViewConnector
+        
+        viewController.presenter = presenter
+        //TODO: vc.connector = connector
+        
+        interactor.presenter = presenter
+        
+        return viewController
+    }
+}
