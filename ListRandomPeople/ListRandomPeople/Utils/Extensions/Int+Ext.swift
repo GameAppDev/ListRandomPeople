d3c1d005906c93d6862688ddd3b5ca2eb81fc6dd
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-03-25 21:02:10 +0300

    Int+Ext is added.

---
+++ ListRandomPeople/ListRandomPeople/Utils/Extensions/Int+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  Int+Ext.swift
+//  ListRandomPeople
+//
+//  Created by Oguzhan Yalcin on 25.03.2023.
+//
+
+import Foundation
+
+extension Int {
+    
+    func toString() -> String {
+        return "\(self)"
+    }
+}
